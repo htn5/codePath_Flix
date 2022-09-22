cde1ab7172f0e981f359b939ac2ac803982b7b43
Heather Nguyen <htn@ucsb.edu>
2022-09-22 15:29:02 -0700

    finished reqs for flix2

---
+++ Flix 2/Flix 2/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  Flix 2
+//
+//  Created by Heather Nguyen on 9/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
